bee190341330f177a1b4a91edf9f72b01aecaa3e
sinamoraddar <sinamoraddar@gmail.com>
2021-01-30 16:30:07 +0330

    change the profile image

--- src/components/sidebar/profile-image.js
+++ src/components/sidebar/profile-image.js
@@ -3,16 +3,18 @@ import React from "react";
 import gravatar from "gravatar";
 import { ProfileType } from "../../types";
 
-const ProfileImage = ({ image, name }) => (
-  <div className="relative inline-block leading-none">
-    <Img
-      fixed={image.childImageSharp.fixed}
-      alt={name}
-      className="rounded-full relative z-10"
-    />
-    <div className="absolute inset-0 rounded-full bg-lead z-0 transform translate-x-12"></div>
-  </div>
-);
+const ProfileImage = ({ image, name }) => {
+  return (
+    <div className="relative inline-block leading-none">
+      <img
+        src={gravatar.url("sinamoraddar@gmail.com", { s: 180 })}
+        alt={name}
+        className="rounded-full relative z-10"
+      />
+      <div className="absolute inset-0 rounded-full bg-lead z-0 transform translate-x-12"></div>
+    </div>
+  );
+};
 
 ProfileImage.propTypes = {
   image: ProfileType.image,
